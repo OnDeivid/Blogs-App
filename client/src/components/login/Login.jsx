c0243c17bce227bfaacb89a0b200ecd3fe9df214
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-01-14 22:02:00 +0200

    Correct spelling mistake in [Login.jsx and Login.css/login]

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -2,10 +2,10 @@ import './Login.css'
 export default function Login() {
     return (<form className="loginForm">
         <h2>LOGN</h2>
-        <div className="inputEmailHeandler">
+        <div className="inputEmailHandler">
             <input type="text" placeholder="email"></input>
         </div>
-        <div className="inputPasswordHeandler">
+        <div className="inputPasswordHandler">
             <input type="password" placeholder="password"></input>
         </div>
         <button className='onLoginSubmit' type='submit'>LOGIN</button>
