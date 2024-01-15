30f721c049e0694443e24b4f9637a53f360cf729
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-01-15 20:05:14 +0200

    Adding responsive functionality for the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import Header from './components/header/Header'
 import Login from './components/login/Login'
 import Register from './components/register/Register'
-
 function App() {
 
   return (
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,14 +1,42 @@
+import { useEffect, useState } from 'react'
+import { useMediaQuery } from 'react-responsive';
 import './Header.css'
 export default function Header() {
+    const [navbarState, setNavbar] = useState(false);
+
+    const isMobile = useMediaQuery({ maxWidth: 500 });
+
+    useEffect(() => {
+        if (!isMobile && window.innerWidth > 500) {
+            setNavbar(true)
+        }
+        if (isMobile && window.innerWidth <= 500) {
+            setNavbar(false)
+        }
+    }, [isMobile])
+    function onMobileToggle() {
+        setNavbar((prev) => !prev);
+    }
+
     return (
         <nav>
+            <a className='toggle-button'>
+                <span className='bar' onClick={onMobileToggle}>M</span>
+            </a>
             <ul>
-                <li><a to="/login">Login</a></li>
-                <li><a to="/register">Register</a></li>
-                <li><a to="/">home</a></li>
-                <li><a to="/catalog">Catalog</a></li>
-                <li><a to="/logout">Logout</a></li>
-                <li><a to="/profile">Profile</a></li>
+                {navbarState ? <>
+                    <li><a to="/login">Login</a></li>
+                    <li><a to="/register">Register</a></li>
+                    <li><a to="/">home</a></li>
+                    <li><a to="/catalog">Catalog</a></li>
+                    <li><a to="/logout">Logout</a></li>
+                    <li><a to="/profile">Profile</a></li>
+                </>
+                    :
+                    <>
+                        <li><a></a></li>
+                    </>}
+
             </ul>
         </nav>
     )
