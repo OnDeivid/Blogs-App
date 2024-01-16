91466030e37f90df5f9fc542aa9897db74bb95a2
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-01-16 23:47:23 +0200

    Adding CORS middleware and handling register data on the server

--- server/CONSTANTS.js
+++ server/CONSTANTS.js
@@ -1,9 +1,15 @@
 const PORT = 3000
 
 
+const AuthEndpoints = {
+    LOGIN: '/login',
+    REGISTER: '/register',
+    LOGOUT: '/logout'
+}
 
 
 
 module.exports = {
-    PORT
+    PORT,
+    AuthEndpoints
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
-
-const { PORT } = require('./CONSTANTS')
-
+const cors = require('cors')
+const { PORT, AuthEndpoints } = require('./CONSTANTS')
+AuthEndpoints
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.post(AuthEndpoints.REGISTER, (req, res) => {
+    res.status(200).json({ status: 'successfully' });
+});
 app.listen(PORT, () => console.log('Server is working...'))
