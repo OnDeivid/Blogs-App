d63b9f99adc3e8e73f7562a1d07ddd8b14a23e06
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-01-27 15:01:31 +0200

    fix css bug

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -44,23 +44,19 @@ export default function Header() {
                 <span className='bar' onClick={onMobileToggle}>M</span>
             </Link>
             <ul>
-<<<<<<< HEAD
-                
-                <li><Link to="/"><img className='logo-home' src='https://seeklogo.com/images/D/d3-logo-c1884590DC-seeklogo.com.png'></img></Link></li>
-=======
-                <img src=''></img>
                 <li ><Link to="/"><img className='logo-home' src='https://seeklogo.com/images/D/d3-logo-C1884590DC-seeklogo.com.png'></img></Link></li>
->>>>>>> ac047b98ea1fc8b7e7159602ea0ad4cc03e12698
+
                 {navbarState ?
                     auth?._id ?
                         <>
+                            <li><Link to="/">HOME</Link><div className='underLine'></div></li>
                             <li><Link to="/catalog">CATALOG</Link><div className='underLine'></div></li>
-
                             <li><Link to="/profile">PROFILE</Link><div className='underLine'></div></li>
                             <li onClick={onLgout}><Link to="/logout">LOGOUT</Link><div className='underLine'></div></li>
                         </>
                         :
                         <>
+                            <li><Link to="/">HOME</Link><div className='underLine'></div></li>
                             <li><Link to="/register">REGISTER</Link><div className='underLine'></div></li>
                             <li><Link to="/login">LOGIN</Link><div className='underLine'></div></li>
                         </>
