a6f4c18e2b8f1181c2a49b3cd454f7d6611856e8
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-02-09 23:00:05 +0200

    big changes

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,32 +1,40 @@
+import { useState } from 'react'
 import './home.css'
 export default function Home() {
+    const [sliderValue, setSliderValue] = useState(1)
+
+    function onChangeRange(e) {
+        setSliderValue(e.target.value)
+    }
+    console.log(sliderValue)
     return (
         <>
             <div className="slideShow">
                 <div className='slide-wrapper'>
+                    <input className='currentCar' onChange={onChangeRange} type='range' min='1' max='50'></input>
                     <div className='slide'>
                         <div className='carModelSponFront'>
                             <a className='modelName'>RS e-tron GT</a>
                         </div>
-                        <div className='carModelSponBack'>
-                            <div>
-                                <h3>Top speed</h3>
-                                <p className='summary'>250 kph</p>
-                            </div >
-                            <div >
-                                <h3>Acceleration 0-100 km/h</h3>
-                                <p className='summary'>3.3 Seconds</p>
-                            </div>
-                            <div>
-                                <h3>Gearbox</h3>
-                                <p className='summary'>2-speed / automatic</p>
-                            </div>
-
-                        </div>
-                        <img className='carImg' src='https://www.audi.com.au/content/dam/nemo/australia/model-overview/thumbnail/e-tron/962x540-RS-e-tron_GT_v2_240222.png?imwidth=563'></img>
+                        <img className='carImg' src={`./porche/${sliderValue}.png`}></img>
                     </div>
+                    <img className='backPodium' src='./porche/backPodium2.png'></img>
                 </div>
             </div>
         </>
     )
 }
+{/* <div className='carModelSponBack'>
+    <div className='el'>
+        <h3>Top speed</h3>
+        <p className='summary'>250 kph</p>
+    </div>
+    <div className='el'>
+        <h3>Acceleration 0-100 km/h</h3>
+        <p className='summary'>3.3 Seconds</p>
+    </div>
+    <div className='el'>
+        <h3>Gearbox</h3>
+        <p className='summary'>2-speed / automatic</p>
+    </div>
+</div> */}
