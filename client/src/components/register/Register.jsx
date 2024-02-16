3705dbb02b31712aff379007111b090f71aae604
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-02-16 11:27:17 +0200

    fix tiny bug

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 
-import FirstForm from './formsRegister/firstLayer'
+import FirstForm from './formsRegister/FirstLayer'
 import SecondForm from './formsRegister/SecondLayer'
 import { AuthEndpoints } from '../../CONSTANTS'
 import axios from '../../api/axios'
