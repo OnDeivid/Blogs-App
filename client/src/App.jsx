e262b7a88c6cd0f6e067dfe0c1f89732adc090a0
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-04-04 10:07:14 +0300

    Catalog Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,41 @@
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
-import Potat from './components/details/Details'
-import Header from './components/header/Header'
-import Login from './components/login/Login'
-import Register from './components/register/Register'
-import Details from './components/details/Details'
-import Catalog from './components/catalog/Catalog'
-import Home from './components/home/Home'
+import Header from './components/header/Header';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+import Details from './components/details/Details';
+import Catalog from './components/catalog/Catalog';
+import Home from './components/home/Home';
 
-import { useSessionStorage } from './hook/useSessionStorage'
-import { authContext } from './context/authContext'
+import { useSessionStorage } from './hook/useSessionStorage';
+import { authContext } from './context/authContext';
 
-import useLocalStorageTheme from './hook/useThemeHook'
+import useLocalStorageTheme from './hook/useThemeHook';
 
-import './App.css'
+import './App.css';
 
 function App() {
-
-  const { theme, toggleTheme } = useLocalStorageTheme('dark')
-  const [auth, setAuth] = useSessionStorage('token', {})
-
-  document.body.className = theme == 'dark' ? 'dark-mode' : 'light-mode'
-
+  const { theme, toggleTheme } = useLocalStorageTheme('dark');
+  const [auth, setAuth] = useSessionStorage('token', {});
 
   const contex = {
     auth, setAuth, theme, toggleTheme
-  }
+  };
+
   return (
-
     <authContext.Provider value={contex}>
-      <Header />
-      <Routes>
-
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/catalog' element={<Catalog />}></Route>
-        <Route path='/details/:id' element={<Details />}></Route>
-
-      </Routes >
+      <div className={theme === 'dark' ? 'dark-mode' : 'light-mode'}>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/catalog' element={<Catalog />}></Route>
+          <Route path='/details/:id' element={<Details />}></Route>
+        </Routes>
+      </div>
     </authContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,9 +1,19 @@
 import FilterCatalog from "./FilterCatalog"
+import CatalogCard from './CatalogCard'
 
+import './Catalog.css'
 export default function Catalog() {
+
     return (
         <>
             <FilterCatalog />
+            <div className="catalogContainer">
+                <div className="catalogHolder">
+                    <CatalogCard />
+                    <CatalogCard />
+                    <CatalogCard />
+                </div>
+            </div>
         </>
     )
 }
