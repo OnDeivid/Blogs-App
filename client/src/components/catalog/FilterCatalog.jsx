5156c0edd733e86a7e5110f449c6c0835f61b405
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-04-04 10:08:24 +0300

    fix btn-ai and btn-bits css

--- client/src/components/catalog/FilterCatalog.jsx
+++ client/src/components/catalog/FilterCatalog.jsx
@@ -8,35 +8,35 @@ export default function FilterCatalog() {
             <div className='holder_options'>
                 <div className='optionContainer'>
 
-                    <label for="brand">Brand:</label>
+                    <label htmlFor="brand">Brand:</label>
                     <select id="brand">
                         <option value="toyota">Toyota</option>
                         <option value="ford">Ford</option>
                     </select>
 
-                    <label for="brand">Model:</label>
+                    <label htmlFor="brand">Model:</label>
                     <select id="brand">
                         <option value="toyota">Toyota</option>
                         <option value="ford">Ford</option>
                     </select>
 
-                    <label for="condition">Condition:</label>
+                    <label htmlFor="condition">Condition:</label>
                     <select id="condition">
                         <option value="new">New</option>
                         <option value="used">Used</option>
                     </select>
 
 
-                    <label for="mileage">Mileage:</label>
+                    <label htmlFor="mileage">Mileage:</label>
                     <input type="text" id="mileage" placeholder="Enter mileage"></input>
 
-                    <label for="fuel">Fuel:</label>
+                    <label htmlFor="fuel">Fuel:</label>
                     <select id="fuel">
                         <option value="gas">Gasoline</option>
                         <option value="diesel">Diesel</option>
                     </select>
 
-                    <label for="year">Year:</label>
+                    <label htmlFor="year">Year:</label>
                     <select id="year">
                         <option value="2022">2022</option>
                         <option value="2021">2021</option>
